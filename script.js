783ba3f0a55a48788a33317422b1855b8447ced7
lawrencelonghi <lawlonghi@gmail.com>
2025-02-14 12:46:58 -0300

    countdown timer with reset button and stop button preventing a second start

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const counter = document.getElementById('counter');
 const startBtn = document.getElementById('start-btn');
 const resetBtn = document.getElementById('reset-btn');
+const stopBtn = document.getElementById('stop-btn')
+let intervalId
+
 
 function startCountdown(){
 const number = +counter.innerHTML
+if(number > 0){
 counter.textContent = number - 1
+}else { clearInterval(intervalId)}
 }
+ startBtn.addEventListener('click', ()=>{
+  if (intervalId) return
+  intervalId = setInterval((startCountdown),1000)})
 
-startBtn.addEventListener('click', ()=>{
-  setTimeout((startCountdown),1000)
+resetBtn.addEventListener('click', () =>{
+  clearInterval(intervalId)
+  intervalId = null
+  counter.textContent = 10
+})
+stopBtn.addEventListener('click', () =>{
+  clearInterval(intervalId)
+  intervalId = null;
+  counter.textContent = innerHTML
 })
